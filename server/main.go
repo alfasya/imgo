e0ec327ae97e00cd3f4ef6c0792966701fc86beb
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-05-03 00:20:43 +0800

    edit module

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/alfasya/imgo/db"
 	"github.com/alfasya/imgo/handlers"
 )
 
@@ -10,6 +11,8 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /auth/register", handlers.Register)
+
+	db.Connect()
 
 	http.ListenAndServe(":8080", mux)
 }
