062ae49f56f24d2ddf67be660a6a02ae17d91ddc
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-05-14 11:46:41 +0800

    create client side

---
+++ client/script.js
@@ -0,0 +1,37 @@
+let form = document.getElementById("form")
+let images = document.getElementById("images")
+let formContainer = document.getElementById("form-container")
+const response = document.createElement("p")
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Nzg3MjkzODcsInVzZXJJZCI6NywidXNlcm5hbWUiOiJzeW4ifQ.TuJxGSjQlKT6EIY6YrxpJtmDQA0RRNiZKfiYmF67sCE"
+
+    const formData = new FormData()
+
+    for (let i = 0; i < images.files.length; i++) {
+        formData.append("images", images.files[i])
+    }
+
+    try {
+        let res = await fetch("http://localhost:8080/upload", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            body: formData,
+        })
+
+        response.replaceChildren()
+
+        let data = await res.json()
+
+        response.textContent = data.message
+        formContainer.appendChild(response)
+
+    } catch (err) {
+        console.log(err)
+        return
+    }
+})
