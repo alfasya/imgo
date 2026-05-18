72571078bf613d1138aecccaf6fff67a86873427
Rifat Alfasya <rifatalfasya.eng@gmail.com>
2026-05-18 21:45:54 +0800

    edit auth page

--- client/auth/script.js
+++ client/auth/script.js
@@ -1,9 +1,10 @@
-let auth = document.getElementById("auth")
+//NEW
 let login = document.getElementById("login")
 let register = document.getElementById("register")
 let content  = document.getElementById("content")
 let authForm = document.getElementById("auth-form")
-let authSubmit = document.getElementById("auth-submit")
+let loginSubmit = document.getElementById("auth-submit-login")
+let regisSubmit = document.getElementById("auth-submit-register")
 let text = document.createElement("p")
 
 const token = localStorage.getItem("token")
@@ -12,121 +13,102 @@ if (token != null) {
     window.location.replace("http://localhost:5500/client/gallery")
 }
 
-let state = 0;
-
 text.setAttribute("class", "regis-info")
 authForm.appendChild(text)
 authForm.prepend(text)
 text.textContent = "You have to log in to access your gallery or upload images"
 
-login.addEventListener("click", async (e) => {
-    e.preventDefault()
-
-    state = 0
-
+login.addEventListener("click", () => {
+    regisSubmit.style.display = "none"
+    loginSubmit.style.display = "block"
     login.setAttribute("class", "auth-buttons login-active")
     authForm.style.setProperty("border-top", "3px solid #3885cd")
-    register.classList.remove("register-active")
-    authSubmit.textContent = "Login"
+    register.setAttribute("class", "auth-buttons")
+
+})
+register.addEventListener("click", () => {
+    regisSubmit.style.display = "block"
+    loginSubmit.style.display = "none"
+    register.setAttribute("class", "auth-buttons register-active")
+    authForm.style.setProperty("border-top", "3px solid #cd4438")
+    login.setAttribute("class", "auth-buttons")
+})
+
+let state = "login"
+
+const response = {
+    ok: "Logged in",
+    notOk: "Wrong username or password",
+}
+
+loginSubmit.addEventListener("click", () => {
+    state = "login"
+    ok = "Logged in"
+    notOk = "Wrong username or password"
+})
+
+regisSubmit.addEventListener("click", () => {
+    state = "register"
+    response.ok = "Registered Successfully"
+    response.notOk = "Username already exists"
 })
 
 authForm.addEventListener("submit", async (e) => {
     e.preventDefault()
 
-    try {
-        const formData = new FormData(authForm)
+    let uri
 
-        const form = {
-            username: formData.get("username"),
-            password: formData.get("password"),
-        }
-
-        let res = await fetch("http://localhost:8080/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(form),
-        })
-
-        if (res.status != 200) {
-            console.log("wrong username or password")
-            
-            authForm.appendChild(text)
-            authForm.prepend(text)
-            text.textContent = "Wrong username or password"
-            return
-        }
-
-        const data = await res.json()
-        console.log(data)
-
-        localStorage.setItem("token", data.token)
-        localStorage.setItem("username", data.username)
-
-        text.setAttribute("class", "regis-info")
-        authForm.appendChild(text)
-        authForm.prepend(text)
-        text.textContent = "Logged in"
-
-        window.location.replace("http://localhost:5500/client/gallery")
-
-    } catch(err) {
-        console.log(err)
+    if (state === "login") {
+        uri = "http://localhost:8080/login"
+    } else if (state === "register") {
+        uri = "http://localhost:8080/register"
+    } else {
+        console.log("error: uri not found")
         return
     }
-})
 
-register.addEventListener("click", async (e) => {
-    e.preventDefault()
+    const formData = new FormData(authForm)
 
-    register.setAttribute("class", "auth-buttons register-active")
-    authForm.style.setProperty("border-top", "3px solid #cd4438")
-    login.classList.remove("login-active")
-    authSubmit.textContent = "Register"
+    const form = {
+        username: formData.get("username"),
+        password: formData.get("password"),
+    }
 
-    authForm.addEventListener("submit", async (e) => {
-        e.preventDefault()
-
-        text.remove()
-
-        try {
-            const formData = new FormData(authForm)
-
-            const form = {
-                username: formData.get("username"),
-                password: formData.get("password"),
-            }
-
-            let res = await fetch("http://localhost:8080/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(form),
-            })
-
-            if (res.status != 201) {
-                console.log("username already exists")
-
-                text.setAttribute("class", "regis-info")
-                authForm.appendChild(text)
-                authForm.prepend(text)
-                text.textContent = "Username already exists"
-                return
-            }
-
-            const data = await res.json()
-            console.log(data)
-            text.setAttribute("class", "regis-info")
-            authForm.appendChild(text)
-            authForm.prepend(text)
-            text.textContent = "Registered successfully"
-
-            location.reload()
-        } catch(err) {
-            console.log(err)
-            return
-        }
+    const res = await fetch(uri, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(form)
     })
+
+    console.log(form)
+    console.log(res)
+
+    if (!res.ok) {
+        console.log(response.notOk)
+        
+        authForm.appendChild(text)
+        authForm.prepend(text)
+        text.textContent = response.notOk
+        return     
+    }
+
+    const data = await res.json()
+
+    if (state === "login") {
+        localStorage.setItem("token", data.token)
+        localStorage.setItem("username", data.username)     
+    }
+
+    text.setAttribute("class", "regis-info")
+    authForm.appendChild(text)
+    authForm.prepend(text)
+    text.textContent = response.ok
+
+    if (state === "login") {
+        window.location.replace("http://localhost:5500/client/gallery")
+    } else if (state === "regis") {
+        location.reload()
+    }
 })
